e24e593c2a5ead75bee8b88b3501088a765fe429
diegofergamboa <diegofergamboa@hotmail.es>
2023-08-22 19:56:42 -0500

    TS explanation added

--- src/02-demo2.ts
+++ src/02-demo2.ts
@@ -1,3 +1,19 @@
+// NPX tsc --init
 // With the configuration of the tsconfig.json we can especify the file and the rout of the compile destination
+
+//* Running the command npx tsc we can just let the configuration running.
+
+// This code showcases the basic usage of TypeScript and explains each step.
+
+// To initialize a TypeScript configuration file, run: npx tsc --init
+// This creates a 'tsconfig.json' file that holds compiler settings.
+
+// In TypeScript, we can specify types explicitly for variables, enhancing code clarity.
+
+// An array of numbers is declared with the type annotation 'number[]'.
+
 const numbers = [1,2,3,4,5]
 console.log(numbers)
+
+// To compile TypeScript code to JavaScript, simply run: npx tsc
+// TypeScript compiler uses 'tsconfig.json' for compilation settings.
