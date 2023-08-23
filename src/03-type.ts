9f79e83d485853358237b154d6e4e72b89793a7d
diegofergamboa <diegofergamboa@hotmail.es>
2023-08-22 20:05:09 -0500

    Definition of type

---
+++ src/03-type.ts
@@ -0,0 +1,4 @@
+// We can use the flexibility of the flexible type and use the declaration of the type. For example in the next variable we made the normal declaration, but at the same type we declare what kind of variable type it is
+
+const myNumber : number = 20
+const myString : string = 'Diego'
