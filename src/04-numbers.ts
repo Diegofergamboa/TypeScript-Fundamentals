61e641101bc2f984caccc5547cce3a4860bc0b6f
diegofergamboa <diegofergamboa@hotmail.es>
2023-08-22 20:39:19 -0500

    Number type studied

---
+++ src/04-numbers.ts
@@ -0,0 +1,22 @@
+(() => {
+    // With the inference we can assign the type of number for the variable
+    let productPrice = 100
+    productPrice = 150
+    console.log(productPrice)
+
+    // But we can use the tsc alert with operations with not valid types as sum an string with a number
+    let customerAge : number = 26
+    customerAge = customerAge + 1
+    console.log(`The customer age is: ${customerAge}`)
+
+    // To initialize the type of variable we should use the type of variable as in the next example
+    let productInStock: number
+    console.log(`product in stock ${productInStock}`)
+
+    if (productInStock > 10) {
+        console.log(`Is greater than 10`)
+    }
+
+    let discount = parseInt('123')
+    console.log(`Discount: ${discount}`)
+})()
