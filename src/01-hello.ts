d7730d791d12c6d1969cc45123cabeb88b3ba938
diegofergamboa <diegofergamboa@hotmail.es>
2023-08-22 19:53:14 -0500

    TS Config set

---
+++ src/01-hello.ts
@@ -0,0 +1,5 @@
+// On typescript we transpiled the code.
+// Itś a good practice to create a file dist
+// We can createwith this code "tsc /file --target es6 --outDist /distRout"
+const myName = 'Diego'
+console.log(myName)
---
+++ src/02-demo2.ts
@@ -0,0 +1,3 @@
+// With the configuration of the tsconfig.json we can especify the file and the rout of the compile destination
+const numbers = [1,2,3,4,5]
+console.log(numbers)
