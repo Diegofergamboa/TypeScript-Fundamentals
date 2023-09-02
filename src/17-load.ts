c24cb8a6372c8925f57ca775464c5a5b4aff93b8
diegofergamboa <diegofergamboa@hotmail.es>
2023-09-02 18:26:24 -0500

    Load is a geat library for help with the group data

--- src/17-load.ts
+++ src/17-load.ts
@@ -1,3 +1,26 @@
 // The library using the underline works to use it into TS
 // To work with that types for manually work with external libraries
 import _ from 'lodash';
+
+const data = [
+	{
+		username: 'Diego',
+		role: 'Admin',
+	},
+	{
+		username: 'Fernando',
+		role: 'Expert',
+	},
+	{
+		username: 'Ortiz',
+		role: 'Admin',
+	},
+	{
+		username: 'Gamboa',
+		role: 'Sales',
+	},
+];
+
+// Using now the manually types we can check the products
+const rta = _.groupBy(data, (item) => item.role);
+console.log(rta);
